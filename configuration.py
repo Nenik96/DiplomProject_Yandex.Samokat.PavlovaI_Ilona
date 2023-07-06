0fb01638a24b1635ad450b7ab92b51f9f2715980
Nenik96 <134986742+Nenik96@users.noreply.github.com>
2023-07-06 13:42:40 -0600

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://d6ce5aff-d4f5-4646-ae66-40127f828f71.serverhub.praktikum-services.ru/"
+CREATE_ORDER_PATH = "/api/v1/orders/"
+GET_ORDER_PATH = "/api/v1/orders/track?t="
---
+++ main.py
@@ -0,0 +1,8 @@
+headers = {
+    "Content-Type": "application/json"
+}
+{
+    "login": "nenik",
+    "password": "1234",
+    "firstName": "ilona"
+}
